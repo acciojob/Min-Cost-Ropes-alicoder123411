aec8bc32d591a9f3c49a9fad2817a58c85ec1eb8
alicoder123411 <sameersingla240@gmail.com>
2026-01-19 19:52:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function mincost(arr)
-{ 
+{   let cost = 0;
+
+    while (arr.length > 1) {
+        // Sort array in ascending order
+        arr.sort((a, b) => a - b);
+
+        // Take two smallest ropes
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let sum = first + second;
+        cost += sum;
+
+        // Push the combined rope back
+        arr.push(sum);
+    }
+    return cost;
+}
 //write your code here
 // return the min cost
-  
-}
-
 module.exports=mincost;
